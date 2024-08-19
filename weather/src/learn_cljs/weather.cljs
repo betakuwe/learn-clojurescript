e47e4f4300439c8b105de540faac8d016009ffda
Benjamin Tan <benjamin.tan@klasses.com.sg>
2024-08-19 17:06:40 +0800

    start of capstone1

--- weather/src/learn_cljs/weather.cljs
+++ weather/src/learn_cljs/weather.cljs
@@ -3,36 +3,16 @@
             [reagent.core :as reagent :refer [atom]]
             [reagent.dom :as rdom]))
 
-(println
-  "This text is printed from src/learn_cljs/weather.cljs. Go ahead and edit it and see reloading in action.")
-
-(defn multiply [a b] (* a b))
-
 ;; define your app data so that it doesn't get over-written on reload
 (defonce app-state (atom {:text "Hello world!"}))
 
-(defn get-app-element [] (gdom/getElement "app"))
-
 (defn hello-world
   []
-  [:div [:h1 "I say: " (:text @app-state)]
-   [:p "I also say: " (:text @app-state)]
-   [:h3 "Edit this in src/learn_cljs/weather.cljs and watch it change!"]])
+  [:div [:h1 {:class "app-title"} "Hello, World"]])
 
-(defn greeter [] [:div "Hello, <YOUR_NAME>"])
-
-(defn append-element
-  [parent child]
-  (when-not (.contains parent child) (.appendChild parent child)))
-
-(defonce is-initialized?
-  (do (.setItem js/localStorage "init-at" (.now js/Date))
-      (js/alert "Welcome!")
-      true))
-
-(defn mount [el] (rdom/render [greeter] el))
-
-(defn mount-app-element [] (when-let [el (get-app-element)] (mount el)))
+(defn mount-app-element []
+  (when-let [el (gdom/getElement "app")]
+    (rdom/render [hello-world] el)))
 
 ;; conditionally start your application based on the presence of an "app"
 ;; element
@@ -46,4 +26,4 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
