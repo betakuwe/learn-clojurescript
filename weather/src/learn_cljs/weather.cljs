819df2b2f60ab8f5987aa5a13901da893e46b077
Benjamin Tan <benjamin.tan@klasses.com.sg>
2024-08-20 17:57:15 +0800

    done capstone1

--- weather/src/learn_cljs/weather.cljs
+++ weather/src/learn_cljs/weather.cljs
@@ -1,21 +1,51 @@
 (ns ^:figwheel-hooks learn-cljs.weather
   (:require [goog.dom :as gdom]
             [reagent.core :as reagent :refer [atom]]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rdom]
+            [cljs.core :as c]
+            [ajax.core :as ajax]))
 
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:title "WhichWeather"
-                          :postal-code ""
-                          :temperatures {:today {:label "Today" :value nil}
-                                         :tomorrow {:label "Tomorrow" :value nil}}}))
+(defonce app-state (reagent/atom {:title "WhichWeather"
+                                  :postal-code ""
+                                  :temperatures {:temp {:label "Temperature" :value nil}
+                                                 :temp-min {:label "Min" :value nil}
+                                                 :temp-max {:label "Max" :value nil}}}))
+
+(defn handle-response [resp]
+  (let [{temp "temp"  temp-min "temp_min"  temp-max "temp_max"} (resp "main")]
+    (swap! app-state update-in [:temperatures :temp :value] (c/constantly temp))
+    (swap! app-state update-in [:temperatures :temp-min :value] (c/constantly temp-min))
+    (swap! app-state update-in [:temperatures :temp-max :value] (c/constantly temp-max))))
+
+(def ^:private api-key "b14b3ae77dacd859192ea3181697e03d")
+
+(defn get-geo-encoding! [postal-code response-handler]
+  (ajax/GET "http://api.openweathermap.org/geo/1.0/zip"
+    {:params {"zip" (str postal-code ",US")
+              "appid" api-key}
+     :handler response-handler}))
+
+(defn get-forecast!
+  ([lat lon response-handler]
+   (ajax/GET "http://api.openweathermap.org/data/2.5/weather"
+     {:params {"lat" lat
+               "lon" lon
+               "appid" api-key}
+      :handler response-handler}))
+  ([postal-code response-handler]
+   (get-geo-encoding! postal-code
+                      #(let [lat (% "lat")
+                             lon (% "lon")]
+                         (get-forecast! lat lon response-handler)))))
 
 (defn title []
   [:h1 (:title @app-state)])
 
 (defn temperature [temp]
   [:div {:class "temperature"}
-   [:div {:class "value"}]
-   [:h2 (:label temp)]])
+   [:div {:class "value" :key "value"} (:value temp)]
+   [:h2 {:key "temp"} (:label temp)]])
 
 (defn postal-code []
   [:div {:class "postal-code"}
@@ -23,15 +53,19 @@
    [:input {:type "number"
             :placeholder "Postal Code"
             :value (:postal-code @app-state)
-            :on-change #(swap! app-state assoc :postal-code (-> % .-target .-value))}]
-   [:button "Go"]])
+            :on-change
+            (fn [event]
+              (swap! app-state assoc :postal-code (-> event .-target .-value)))}]
+   [:button
+    {:on-click #(get-forecast! (:postal-code @app-state) handle-response)}
+    "Go"]])
 
 (defn app []
   [:div {:class "app"}
    [title]
    [:div {:class "temperatures"}
     (for [temp (vals (:temperatures @app-state))]
-      [temperature temp])]
+      [temperature temp {:key (:label temp)}])]
    [postal-code]])
 
 (defn mount-app-element []
